2282df5445fbabdaac484b9f0e5a5e22953dde5d
Peter Evans <peter-evans@users.noreply.github.com>
2020-06-24 12:00:13 +0000

    autopep8 action fixes

--- src/wordfeud_logic/wordlist.py
+++ src/wordfeud_logic/wordlist.py
@@ -47,10 +47,12 @@ class Wordlist(object):
             self.word_count += 1
 
     def words(self, row, rowdata, letters, variant):
-        assert (len(row) == len(rowdata)), ("%d == %d" % (len(row), len(rowdata)))
-        row = row+' '
-        for pos in range(len(row)-1):
-            if pos > 0 and row[pos-1] != ' ':
+        assert (
+            len(row) == len(rowdata)), ("%d == %d" %
+                                        (len(row), len(rowdata)))
+        row = row + ' '
+        for pos in range(len(row) - 1):
+            if pos > 0 and row[pos - 1] != ' ':
                 continue
             yield from ((pos, word) for word in self.root.matches(row, rowdata, pos, letters, variant))
 
@@ -58,7 +60,8 @@ class Wordlist(object):
         if word == ' ':
             return self.all_chars
         i = word.find(' ')
-        m = list(self.root.matches(word+' ', [(self.all_chars, True)]*(len(word)+1), 0, '*', variant))
+        m = list(self.root.matches(
+            word + ' ', [(self.all_chars, True)] * (len(word) + 1), 0, '*', variant))
         return set(match[i].lower() for match in m)
 
     def is_word(self, word, variant=1):
@@ -69,11 +72,12 @@ class Wordlist(object):
                 if (node.variants & variant) == 0:
                     return False
             return node.word
-        except:
+        except BaseException:
             return False
 
     def __repr__(self):
-        return '<Worldlist: %d words from "%s">' % (self.word_count, '", "'.join(self.wordfiles))
+        return '<Worldlist: %d words from "%s">' % (
+            self.word_count, '", "'.join(self.wordfiles))
 
 
 class Node(object):
@@ -86,7 +90,7 @@ class Node(object):
     def child(self, char):
         try:
             return self.children[char]
-        except:
+        except BaseException:
             c = Node()
             self.children[char] = c
             return c
@@ -94,28 +98,37 @@ class Node(object):
     def has_child(self, char):
         return self.children.get(char)
 
-    def matches(self, row, rowdata, pos, letters, variant, word='', connecting=False, extending=False):
+    def matches(
+            self,
+            row,
+            rowdata,
+            pos,
+            letters,
+            variant,
+            word='',
+            connecting=False,
+            extending=False):
         if pos < len(row) and (variant & self.variants) != 0:
             if row[pos] != ' ':
                 child = self.children.get(row[pos].lower())
                 if child:
-                    yield from child.matches(row, rowdata, pos+1, letters, variant, word+row[pos], True, extending)
+                    yield from child.matches(row, rowdata, pos + 1, letters, variant, word + row[pos], True, extending)
             else:
                 if self.word and connecting and extending and len(word) > 1:
                     yield word
-                if pos < len(row)-1:
+                if pos < len(row) - 1:
                     valid_chars, connected = rowdata[pos]
                     for i, ch in enumerate(letters):
-                        if not ch in valid_chars and ch != '*':
+                        if ch not in valid_chars and ch != '*':
                             continue
                         if letters.find(ch, 0, i) != -1:
                             continue
                         if ch == '*':
                             # wildcard
-                            next_letters = letters[:i] + letters[i+1:]
+                            next_letters = letters[:i] + letters[i + 1:]
                             for wc, child in self.children.items():
                                 if wc in valid_chars:
-                                    yield from child.matches(row, rowdata, pos+1, next_letters, variant, word+wc.upper(), connecting or connected, True)
+                                    yield from child.matches(row, rowdata, pos + 1, next_letters, variant, word + wc.upper(), connecting or connected, True)
                         child = self.children.get(ch)
                         if child:
-                            yield from child.matches(row, rowdata, pos+1, letters[:i] + letters[i+1:], variant, word+ch, connecting or connected, True)
+                            yield from child.matches(row, rowdata, pos + 1, letters[:i] + letters[i + 1:], variant, word + ch, connecting or connected, True)
