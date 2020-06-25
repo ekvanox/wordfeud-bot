b76707b88847cbea20b806d56317a749c52464a1
Pricehacker <admin@system.gq>
2020-06-25 18:30:01 +0200

    ci(workflow): :green_heart: 4th attempt to integrate commit linting
    i think it might work now!
    
    fix #36

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "body-max-line-length": [0, "always", Infinity],
+    "header-max-length": [1, "always", 80],
     "header-case": [2, "always", "lower-case"],
     "type-case": [2, "always", "lower-case"],
   },
