d980f41f76d0dd2dc5ec74348c127953d29f2d99
Pricehacker <admin@system.gq>
2020-06-25 18:57:30 +0200

    test: :white_check_mark: updated tests
    made more tests for for is_emoji function and converted from assert to assertTrue/assertFalse

--- tests/test_wordfeudbot.py
+++ tests/test_wordfeudbot.py
@@ -1,20 +1,40 @@
 import wordfeudbot.main as wfbot
+import unittest
 
 
-def test_is_emoji():
-    assert wfbot.is_emoji('😁')
-    assert wfbot.is_emoji('😂')
-    assert wfbot.is_emoji('🤣')
-    assert wfbot.is_emoji('😃')
-    assert wfbot.is_emoji('😄')
-    assert wfbot.is_emoji('👩')
-    assert wfbot.is_emoji('👨')
-    assert wfbot.is_emoji('🧑🏿')
-    assert wfbot.is_emoji('🎈')
-    assert wfbot.is_emoji('🎆')
-    assert wfbot.is_emoji('🍋')
-    assert wfbot.is_emoji('🛹')
-    assert wfbot.is_emoji('⚡')
-    assert wfbot.is_emoji('💟')
-    assert wfbot.is_emoji('⏪')
-    assert wfbot.is_emoji('🕕')
+class TestStringMethods(unittest.TestCase):
+
+    def test_is_emoji(self):
+        self.assertTrue(wfbot.is_emoji('😁'))
+        self.assertTrue(wfbot.is_emoji('😂'))
+        self.assertTrue(wfbot.is_emoji('🤣'))
+        self.assertTrue(wfbot.is_emoji('😃'))
+        self.assertTrue(wfbot.is_emoji('😄'))
+        self.assertTrue(wfbot.is_emoji('👩'))
+        self.assertTrue(wfbot.is_emoji('👨'))
+        self.assertTrue(wfbot.is_emoji('🧑🏿'))
+        self.assertTrue(wfbot.is_emoji('🎈'))
+        self.assertTrue(wfbot.is_emoji('🎆'))
+        self.assertTrue(wfbot.is_emoji('🍋'))
+        self.assertTrue(wfbot.is_emoji('🛹'))
+        self.assertTrue(wfbot.is_emoji('⚡'))
+        self.assertTrue(wfbot.is_emoji('💟'))
+        self.assertTrue(wfbot.is_emoji('⏪'))
+        self.assertTrue(wfbot.is_emoji('🕕'))
+        self.assertTrue(wfbot.is_emoji('😃🕕🍋'))
+        self.assertTrue(wfbot.is_emoji('👨🛹'))
+        self.assertTrue(wfbot.is_emoji('🎆😃💟💟💟⏪'))
+        self.assertFalse(wfbot.is_emoji('a'))
+        self.assertFalse(wfbot.is_emoji('b'))
+        self.assertFalse(wfbot.is_emoji('c'))
+        self.assertFalse(wfbot.is_emoji('d'))
+        self.assertFalse(wfbot.is_emoji('!'))
+        self.assertFalse(wfbot.is_emoji('¤'))
+        self.assertFalse(wfbot.is_emoji('👨🛹2121'))
+        self.assertFalse(wfbot.is_emoji('2121👨🛹'))
+        self.assertFalse(wfbot.is_emoji('👨2121🛹'))
+        self.assertFalse(wfbot.is_emoji('njdwe"%¤%&!/'))
+
+
+if __name__ == '__main__':
+    unittest.main()
